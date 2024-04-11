e29dbebdcfe6a6e7314d949428db84e08fae421a
Thiago Ferraz <165716234+Dev-ferraz@users.noreply.github.com>
2024-04-11 20:40:41 -0300

    Update Matematic.java

--- Matematic.java
+++ Matematic.java
@@ -6,31 +6,22 @@ public class Matematic {
 
 	public static void main(String[] args) {
 		
-		
 		Calculadora m = new Calculadora();
-		Scanner s =  new Scanner(System.in);
-			
-				System.out.println("CALCULADORA");
+		System.out.println("CALCULADORA");
 				
-				int soma = m.add(5, 5);
-				System.out.println("Resultado Soma = " + soma);
+		int soma = m.add(5, 5);
+		System.out.println("Resultado Soma = " + soma);
 				
-				int sub0 = m.sub(100, 20);
-				System.out.println("Resultado Subtração = " + sub0);
+		int sub0 = m.sub(100, 20);
+		System.out.println("Resultado Subtração = " + sub0);
 				
-				int mult0 = m.mult(10, 2);
-				System.out.println("Resultado Multiplicação = " + mult0);
+		int mult0 = m.mult(10, 2);
+		System.out.println("Resultado Multiplicação = " + mult0);
+					
+		int dividir0 = m.dividir(10, 2);
+		System.out.println("Resultado Divisão = " + dividir0);	
 				
-				
-				int dividir0 = m.dividir(10, 2);
-				System.out.println("Resultado Divisão = " + dividir0);
-				
-				
-				int raiz0 = m.raiz(16);
-				System.out.println("Resultado Raiz quadrada: " + raiz0);
-				
-			
-	      
+		int raiz0 = m.raiz(16);
+		System.out.println("Resultado Raiz quadrada: " + raiz0);
 	}
-
 }
